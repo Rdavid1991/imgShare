e12f78fb316d61ef94ee651d4f9785bf86443490
rdavid1991 <rdavid3.1991@gmail.com>
2019-09-21 22:25:31 -0500

    Add views

--- src/controllers/images.js
+++ src/controllers/images.js
@@ -8,9 +8,19 @@ const { Image, Comment } = require('../models');
 const ctrl = {};
 
 ctrl.index = async(req, res) => {
+    const viewModel ={ image: {},comments:{}};
     const image = await Image.findOne({ filename: { $regex: String(req.params.image_id) } });
-    const comments = await Comment.find({ image_id: image._id });
-    res.render('image', { image, comments });
+    if(image){
+        image.views = image.views + 1;
+        viewModel.image = image;
+        await image.save();
+        const comments = await Comment.find({ image_id: image._id });
+        viewModel.comments = comments;
+        res.render('image', viewModel);
+    }else {
+        res.redirect("/");
+    }
+  
 };
 
 ctrl.create = (req, res) => {
@@ -59,6 +69,8 @@ ctrl.comment = async(req, res) => {
         newComment.image_id = image._id;
         await newComment.save();
         res.redirect('/images/' + image.uniqueId);
+    }else{
+        res.redirect("/");
     }
 };
 
