ec68f782e9a9502f4e9c093fd2266e1700882c24
rdavid1991 <rdavid3.1991@gmail.com>
2019-10-28 19:55:58 -0500

    Add ascii art

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 
 const express = require('express');
+const arte = require('figlet');
 const config = require('./server/config');
+const app = config(express());
 
 require('./database');
 require('./controllers/local-auth');
 require('colors');
 
-const app = config(express());
+console.log(arte.textSync("Hola Mundo", "Sub-Zero").blue);
+
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`.yellow);
 });
